718f9e03d98b2468568a480393bc7f3c584874d1
Maya-Mostafa <mayamostafa@gmail.com>
2021-10-27 14:55:39 -0400

    solution created

---
+++ src/webparts/myCourses/components/IMyCoursesProps.ts
@@ -0,0 +1,3 @@
+export interface IMyCoursesProps {
+  description: string;
+}
---
+++ src/webparts/myCourses/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMyCoursesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MyCoursesWebPartStrings' {
+  const strings: IMyCoursesWebPartStrings;
+  export = strings;
+}
